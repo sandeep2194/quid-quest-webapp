6dec9f273be66f27b5f7b23dbd414356c86ea835
jaspreet <jkaur003@gmail.com>
2023-10-06 22:10:24 -0400

    done UI of login

--- app/auth/forgot-password/page.js
+++ app/auth/forgot-password/page.js
@@ -1,5 +1,27 @@
-export default function Login() {
+"use client"
+import { useState } from "react"
+import StandardButtonGreen from "@/app/lib/Buttons/StandardButtonGreen"
+import StandardInputWIthLabel from "@/app/lib/Input/StandardInputWIthLabel"
+
+export default function ForgotPassword() {
+
+    const [email, setEmail] = useState('')
     return (
-        <h1>forgotpass pAGE</h1>
-    )
+        <div className="flex flex-col justify-centers space-y-6">
+            <h2 className="text-center text-2xl font-bold text-gray-800 mb-4">Forgot Password</h2>
+
+            <StandardInputWIthLabel
+                id={"email"}
+                label={"Enter email to get reset link "}
+                name={"email"}
+                type={"email"}
+                onChange={e => setEmail(e.target.value)
+                } />
+
+            <div className="h-2" />
+            <StandardButtonGreen
+                btnText={"Send Link"}
+                href={"reset-password"}
+            />
+        </div>)
 }
--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,15 +1,29 @@
 "use client"
 import StandardInputWIthLabel from "@/app/lib/Input/StandardInputWIthLabel";
 import StandardButtonGreen from "@/app/lib/Buttons/StandardButtonGreen";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState(false);
+
+    /*
+        1) handleSubmit
+        2) check from db if user exist and password match
+        3) show error if password does not match
+    */
 
     return (
         <div className="flex flex-col justify-centers space-y-6">
             <h1 className="text-center text-2xl font-bold text-gray-800 mb-4">Login</h1>
+
+            {
+                error ? <p className="mt-1 font-semibold leading-6 text-center text-lg text-red-500">
+                    Your credentials does not match !
+                </p> : <></>
+            }
             <StandardInputWIthLabel
                 id={"email"}
                 label={"Email"}
@@ -23,7 +37,7 @@ export default function Login() {
                 label={"Password"}
                 name={"password"}
                 type={"password"}
-                onChange={e => setEmail(e.target.value)
+                onChange={e => setPassword(e.target.value)
                 } />
 
             <div className="h-2" />
@@ -31,6 +45,10 @@ export default function Login() {
                 btnText={"login"}
                 href={"forgot-password"}
             />
+
+            <Link href="forgot-password" className="mt-1 leading-6 text-right text-sm text-green-500 hover:text-green-600" >
+                Forgot Password?
+            </Link>
 
 
 
