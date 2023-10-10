8b6fdd7856fe824f59a2b33c44d20ffabca61885
jaspreet <jkaur003@gmail.com>
2023-10-10 16:59:05 -0400

    profile pic

--- app/dashboard/Drawer.js
+++ app/dashboard/Drawer.js
@@ -217,7 +217,7 @@ export default function Drawer() {
           <span className="sr-only">Your profile</span>
           <img
             className="h-8 w-8 rounded-full bg-gray-800"
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            src="https://picsum.photos/200"
             alt=""
           />
         </a>
