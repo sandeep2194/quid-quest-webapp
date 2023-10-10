bddf345a4fb33ff20bda1699e2b6a53cdebcffa9
sandeep <sandeepcn998@gmail.com>
2023-10-10 09:03:07 -0400

    setup supabase in project implement in login page

--- app/lib/Buttons/StandardButtonGreen.js
+++ app/lib/Buttons/StandardButtonGreen.js
@@ -1,16 +1,24 @@
+"use client";
 import Link from "next/link";
 
-function StandardButtonGreen({ btnText, href }) {
-
-    return (
-        <Link href={href} className="w-full">
-            <button
-                className={"flex justify-center w-full rounded-lg bg-[#2BB529] hover:bg-green-500 px-3 py-3 text-md font-semibold leading-6 text-white shadow-smfocus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"}
-            >
-                {btnText.toUpperCase()}
-            </button>
-        </Link>
-    )
+function StandardButtonGreen({
+  btnText,
+  href = "",
+  type = "submit",
+  onClick = () => {},
+}) {
+  return (
+    <Link href={href} className="w-full">
+      <button
+        className={
+          "flex justify-center w-full rounded-lg bg-[#2BB529] hover:bg-green-500 px-3 py-3 text-md font-semibold leading-6 text-white shadow-smfocus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        }
+        onClick={onClick}
+      >
+        {btnText.toUpperCase()}
+      </button>
+    </Link>
+  );
 }
 
 export default StandardButtonGreen;
