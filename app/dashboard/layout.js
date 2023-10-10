e0c3d05cab6c2e16e8524d9c5acb17cea958d0c8
jaspreet <jkaur003@gmail.com>
2023-10-09 22:11:25 -0400

    list employee  done, working employee invitation

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { Fragment, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import {
     Bars3Icon,
@@ -12,19 +12,15 @@ import {
     UsersIcon,
     XMarkIcon,
 } from '@heroicons/react/24/outline'
+import { useRouter } from 'next/router'
 
 const navigation = [
-    { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-    { name: 'Team', href: '#', icon: UsersIcon, current: false },
-    { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-    { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-    { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-    { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
-]
-const teams = [
-    { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
-    { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
-    { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
+    { name: 'Dashboard', href: '/dashboard', icon: HomeIcon, current: false },
+    { name: 'Employees', href: '/dashboard/employees', icon: UsersIcon, current: false },
+    { name: 'Expenses', href: '/dashboard/expenses', icon: FolderIcon, current: false },
+    { name: 'Reports', href: '/dashboard/reports', icon: ChartPieIcon, current: false },
+    { name: 'Setting', href: '/dashboard/setting', icon: ChartPieIcon, current: false },
+
 ]
 
 function classNames(...classes) {
@@ -33,6 +29,7 @@ function classNames(...classes) {
 
 export default function DashboardLayout({ children }) {
     const [sidebarOpen, setSidebarOpen] = useState(false)
+    const [currentPage, setCurrentPage] = useState(false);
 
     return (
         <>
@@ -48,7 +45,7 @@ export default function DashboardLayout({ children }) {
                             leaveFrom="opacity-100"
                             leaveTo="opacity-0"
                         >
-                            <div className="fixed inset-0 bg-gray-900/80" />
+                            <div className="fixed inset-0 bg-green-950" />
                         </Transition.Child>
 
                         <div className="fixed inset-0 flex">
@@ -79,7 +76,7 @@ export default function DashboardLayout({ children }) {
                                         </div>
                                     </Transition.Child>
                                     {/* Sidebar component, swap this element with another sidebar if you like */}
-                                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10">
+                                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-green-950 px-6 pb-2 ring-1 ring-white/10">
                                         <div className="flex h-16 shrink-0 items-center">
                                             <img
                                                 className="h-11 w-auto pt-2"
@@ -91,12 +88,12 @@ export default function DashboardLayout({ children }) {
                                             <ul role="list" className="flex flex-1 flex-col gap-y-7">
                                                 <li>
                                                     <ul role="list" className="-mx-2 space-y-1">
-                                                        {navigation.map((item) => (
+                                                        {navigation.map((item, i) => (
                                                             <li key={item.name}>
                                                                 <a
                                                                     href={item.href}
                                                                     className={classNames(
-                                                                        item.current
+                                                                        currentPage
                                                                             ? 'bg-gray-800 text-white'
                                                                             : 'text-gray-400 hover:text-white hover:bg-gray-800',
                                                                         'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
@@ -104,29 +101,6 @@ export default function DashboardLayout({ children }) {
                                                                 >
                                                                     <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
                                                                     {item.name}
-                                                                </a>
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-                                                </li>
-                                                <li>
-                                                    <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-                                                    <ul role="list" className="-mx-2 mt-2 space-y-1">
-                                                        {teams.map((team) => (
-                                                            <li key={team.name}>
-                                                                <a
-                                                                    href={team.href}
-                                                                    className={classNames(
-                                                                        team.current
-                                                                            ? 'bg-gray-800 text-white'
-                                                                            : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                                                                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                                                    )}
-                                                                >
-                                                                    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                                                                        {team.initial}
-                                                                    </span>
-                                                                    <span className="truncate">{team.name}</span>
                                                                 </a>
                                                             </li>
                                                         ))}
@@ -144,7 +118,7 @@ export default function DashboardLayout({ children }) {
                 {/* Static sidebar for desktop */}
                 <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
                     {/* Sidebar component, swap this element with another sidebar if you like */}
-                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
+                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-green-950 px-6">
                         <div className="flex h-16 shrink-0 items-center">
                             <img
                                 className="h-12 w-auto pt-2"
@@ -161,9 +135,9 @@ export default function DashboardLayout({ children }) {
                                                 <a
                                                     href={item.href}
                                                     className={classNames(
-                                                        item.current
-                                                            ? 'bg-gray-800 text-white'
-                                                            : 'text-gray-400 hover:text-white hover:bg-gray-800',
+                                                        currentPage
+                                                            ? 'bg-green-800 text-white'
+                                                            : 'text-gray-400 hover:text-white hover:bg-green-800',
                                                         'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                                                     )}
                                                 >
@@ -174,36 +148,13 @@ export default function DashboardLayout({ children }) {
                                         ))}
                                     </ul>
                                 </li>
-                                <li>
-                                    <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-                                    <ul role="list" className="-mx-2 mt-2 space-y-1">
-                                        {teams.map((team) => (
-                                            <li key={team.name}>
-                                                <a
-                                                    href={team.href}
-                                                    className={classNames(
-                                                        team.current
-                                                            ? 'bg-gray-800 text-white'
-                                                            : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                                                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                                    )}
-                                                >
-                                                    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                                                        {team.initial}
-                                                    </span>
-                                                    <span className="truncate">{team.name}</span>
-                                                </a>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </li>
                                 <li className="-mx-6 mt-auto">
                                     <a
                                         href="#"
-                                        className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-gray-800"
+                                        className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-green-800"
                                     >
                                         <img
-                                            className="h-8 w-8 rounded-full bg-gray-800"
+                                            className="h-8 w-8 rounded-full bg-green-800"
                                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                                             alt=""
                                         />
@@ -216,7 +167,7 @@ export default function DashboardLayout({ children }) {
                     </div>
                 </div>
 
-                <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+                <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-green-950 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
                     <button type="button" className="-m-2.5 p-2.5 text-gray-400 lg:hidden" onClick={() => setSidebarOpen(true)}>
                         <span className="sr-only">Open sidebar</span>
                         <Bars3Icon className="h-6 w-6" aria-hidden="true" />
