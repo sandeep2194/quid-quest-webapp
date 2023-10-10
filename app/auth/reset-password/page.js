7e549d976eb0491f7cde9d2f33dacd87d911082f
sandeep <sandeepcn998@gmail.com>
2023-10-10 16:03:45 -0400

    reset password done

--- app/auth/reset-password/page.js
+++ app/auth/reset-password/page.js
@@ -1,45 +1,75 @@
-"use client"
+"use client";
 import StandardInputWIthLabel from "@/app/lib/Input/StandardInputWIthLabel";
 import StandardButtonGreen from "@/app/lib/Buttons/StandardButtonGreen";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import Link from "next/link";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default function ResetPassword() {
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState(true);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  //   const searchParams = useSearchParams();
+  const supabase = createClientComponentClient();
+  const [success, setSuccess] = useState(false);
 
-    return (
-        <div className="flex flex-col justify-centers space-y-6">
-            <h1 className="text-center text-2xl font-bold text-gray-800 mb-4">Reset your Password</h1>
+  const handleReset = async () => {
+    if (!password) {
+      setError(true);
+      return;
+    }
+    if (password != confirmPassword) {
+      setError(true);
+      return;
+    }
+    const res = await supabase.auth.updateUser({ password: confirmPassword });
+    if (res.error) {
+      setError(true);
+    } else {
+      setSuccess(true);
+    }
+  };
 
-            {
-                error ? <p className="mt-1 font-semibold leading-6 text-center text-lg text-red-500">
-                    Your password does not match !
-                </p> : <></>
-            }
+  return (
+    <div className="flex flex-col justify-centers space-y-6">
+      <h1 className="text-center text-2xl font-bold text-gray-800 mb-4">
+        Reset your Password
+      </h1>
 
-            <StandardInputWIthLabel
-                id={"password"}
-                label={"Password"}
-                name={"password"}
-                type={"password"}
-                onChange={e => setPassword(e.target.value)
-                } />
+      {error ? (
+        <p className="mt-1 font-semibold leading-6 text-center text-lg text-red-500">
+          something went wrong
+        </p>
+      ) : (
+        <></>
+      )}
+      {success ? (
+        <p className="mt-1 font-semibold leading-6 text-center text-lg text-red-500">
+          password updated
+        </p>
+      ) : (
+        <></>
+      )}
 
-            <StandardInputWIthLabel
-                id={"confirmPasssword"}
-                label={"Confirm Password"}
-                name={"pconfirmPassword"}
-                type={"password"}
-                onChange={e => setConfirmPassword(e.target.value)
-                } />
+      <StandardInputWIthLabel
+        id={"password"}
+        label={"Password"}
+        name={"password"}
+        type={"password"}
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
-            <div className="h-2" />
-            <StandardButtonGreen
-                btnText={"Submit"}
-                href={"login"}
-            />
-        </div>
-    )
+      <StandardInputWIthLabel
+        id={"confirmPasssword"}
+        label={"Confirm Password"}
+        name={"pconfirmPassword"}
+        type={"password"}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+
+      <div className="h-2" />
+      <StandardButtonGreen btnText={"Submit"} href={"login"} />
+    </div>
+  );
 }
