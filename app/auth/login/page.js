a63af72f7933f2eec4db86d9cd018f48a7c04259
sandeep <sandeepcn998@gmail.com>
2023-10-09 20:44:19 -0400

    login done

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,57 +1,74 @@
-"use client"
+"use client";
 import StandardInputWIthLabel from "@/app/lib/Input/StandardInputWIthLabel";
-import StandardButtonGreen from "@/app/lib/Buttons/StandardButtonGreen";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
+import signIn from "@/app/firebase/auth/signin";
 
 export default function Login() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
 
-    /*
-        1) handleSubmit
-        2) check from db if user exist and password match
-        3) show error if password does not match
-    */
+  async function handleForm(event) {
+    event.preventDefault();
 
-    return (
-        <div className="flex flex-col justify-centers space-y-6">
-            <h1 className="text-center text-2xl font-bold text-gray-800 mb-4">Login</h1>
+    const { result, error } = await signIn(email, password);
 
-            {
-                error ? <p className="mt-1 font-semibold leading-6 text-center text-lg text-red-500">
-                    Your credentials does not match !
-                </p> : <></>
-            }
-            <StandardInputWIthLabel
-                id={"email"}
-                label={"Email"}
-                name={"email"}
-                type={"email"}
-                onChange={e => setEmail(e.target.value)
-                } />
+    if (error) {
+        setError(error)
+      return console.log(error);
+    }
 
-            <StandardInputWIthLabel
-                id={"passsword"}
-                label={"Password"}
-                name={"password"}
-                type={"password"}
-                onChange={e => setPassword(e.target.value)
-                } />
+    // else successful
+    console.log(result);
+  }
 
-            <div className="h-2" />
-            <StandardButtonGreen
-                btnText={"login"}
-                href={"forgot-password"}
-            />
+  return (
+    <div className="flex flex-col justify-centers space-y-6">
+      <h1 className="text-center text-2xl font-bold text-gray-800 mb-4">
+        Login
+      </h1>
 
-            <Link href="forgot-password" className="mt-1 leading-6 text-right text-sm text-green-500 hover:text-green-600" >
-                Forgot Password?
-            </Link>
+      {error ? (
+        <p className="mt-1 font-semibold leading-6 text-center text-lg text-red-500">
+          Your credentials does not match !
+        </p>
+      ) : (
+        <></>
+      )}
+      <form onSubmit={handleForm}>
+        <StandardInputWIthLabel
+          id={"email"}
+          label={"Email"}
+          name={"email"}
+          type={"email"}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
+        <StandardInputWIthLabel
+          id={"passsword"}
+          label={"Password"}
+          name={"password"}
+          type={"password"}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
+        <div className="h-2" />
+        {/* had to use new button because on running handle form function the StandardButtonGreen was going to the href and funtion was not able to run*/}
+        <button
+                className={"flex justify-center w-full rounded-lg bg-[#2BB529] hover:bg-green-500 px-3 py-3 text-md font-semibold leading-6 text-white shadow-smfocus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"}
+                type="submit"
+            >
+                {"LOGIN"}
+            </button>
 
-        </div>
-    )
+        <Link
+          href="forgot-password"
+          className="mt-1 leading-6 text-right text-sm text-green-500 hover:text-green-600"
+        >
+          Forgot Password?
+        </Link>
+      </form>
+    </div>
+  );
 }
--- app/lib/Buttons/StandardButtonGreen.js
+++ app/lib/Buttons/StandardButtonGreen.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
-function StandardButtonGreen({ btnText, href }) {
+function StandardButtonGreen({ btnText, href='#', onClick=()=>{console.log("btn green clicked")} }) {
 
     return (
         <Link href={href} className="w-full">
             <button
                 className={"flex justify-center w-full rounded-lg bg-[#2BB529] hover:bg-green-500 px-3 py-3 text-md font-semibold leading-6 text-white shadow-smfocus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"}
+                onClick={onClick}
             >
                 {btnText.toUpperCase()}
             </button>
